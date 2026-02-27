9bbdf6c93da987c24443b9dc501e8995f5ec4ba8
AvaLilac <amyshimplays@gmail.com>
2026-02-27 00:37:16 -0500

    Create userbadges.js

---
+++ userbadges.js
@@ -0,0 +1,30 @@
+window.AVIA_USER_BADGES = [
+
+    {
+        name: "Avia Client ownerOwner",
+        icon: "👑",
+        color: "var(--md-sys-color-primary)",
+        users: [
+            "AvaLilac#7432"
+        ]
+    },
+
+    {
+        name: "Idea Suggester",
+        icon: "💎",
+        color: "#ff66cc",
+        users: [
+            "Amelia#2716"
+        ]
+    },
+
+      {
+        name: "NameHere",
+        icon: "💎",
+        color: "#ff66cc",
+        users: [
+            // "User#0002"
+        ]
+    }
+
+];;
