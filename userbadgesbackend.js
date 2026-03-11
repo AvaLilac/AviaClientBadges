7b7f10473f0f5dd49bbe256a6f807ce841b505d1
AvaLilac <amyshimplays@gmail.com>
2026-03-11 19:50:20 -0400

    Update userbadgesbackend.js

--- userbadgesbackend.js
+++ userbadgesbackend.js
@@ -1,57 +1,58 @@
 window.AVIA_USER_BADGES = [
-
     {
-        name: "Avia Client Owner",
-        icon: "👑",
-        color: "var(--md-sys-color-primary)",
-        users: [
+        "name": "Avia Client Owner",
+        "icon": "👑",
+        "color": "var(--md-sys-color-primary)",
+        "users": [
             "AvaLilac#7432"
         ]
     },
-
     {
-        name: " Cat Girl",
-        icon: "😸",
-        color: "#ff66cc",
-        users: [
+        "name": " Cat Girl",
+        "icon": "😸",
+        "color": "#ff66cc",
+        "users": [
             "Amelia#2716"
         ]
     },
-
     {
-        name: "KamiNeko!",
-        icon: "🦄",
-        color: "#916acf",
-        users: [
+        "name": "KamiNeko!",
+        "icon": "🦄",
+        "color": "#916acf",
+        "users": [
             "Kaze#6558"
         ]
     },
-    
     {
-        name: "The GXT",
-        icon: "💎",
-        color: "#A40202",
-        users: [
+        "name": "The GXT",
+        "icon": "💎",
+        "color": "#A40202",
+        "users": [
             "MrGXT_yt#0106"
         ]
     },
-
     {
-        name: "Goat",
-        icon: "🐐",
-        color: "#66399E",
-        users: [
+        "name": "Goat",
+        "icon": "🐐",
+        "color": "#66399E",
+        "users": [
             "Nath#9230"
         ]
     },
-    
     {
-        name: "bunny",
-        icon: "🐇",
-        color: "#FCE4EC",
-        users: [
+        "name": "bunny",
+        "icon": "🐇",
+        "color": "#FCE4EC",
+        "users": [
             "amybunnygirl#0122"
         ]
+    },
+    {
+        "name": "Revite Revived Owner",
+        "icon": "👑",
+        "color": "var(--md-sys-color-primary)",
+        "users": [
+            "Asraye#2120"
+        ]
     }
-
 ];
